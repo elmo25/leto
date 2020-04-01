a332cbe79cb1678b271fc2dd1abb40efd0b4ed06
Виталий Жилкин <vetll25@icloud.com>
2020-04-01 23:49:53 +0500

    feat: section services

--- js/main.js
+++ js/main.js
@@ -1,24 +1,53 @@
 $(document).ready(function() {
-  var mySwiper = new Swiper(".swiper-container", {
+  var mySwiper = new Swiper(".hero__swiper-container", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
 
     // If we need pagination
     pagination: {
-      el: ".swiper-pagination",
+      el: ".hero__swiper-pagination",
       clickable: true
     },
 
     // Navigation arrows
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
+      nextEl: ".hero__swiper-button-next",
+      prevEl: ".hero__swiper-button-prev"
     },
 
     // And if we need scrollbar
     scrollbar: {
       el: ".swiper-scrollbar"
+    }
+  });
+  var swiper = new Swiper(".services__swiper-container", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+      el: ".services__swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50
+      }
     }
   });
 });
